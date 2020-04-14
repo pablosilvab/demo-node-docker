c81f2e4f63e6da462a837b4bd3dbec8d7b0eee53
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-04-14 09:30:58 -0400

    add endpoint

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/', function (req, res) {
   res.send(`Hello ${message}!`);
 });
 
+app.get('/status', (req,res)=>{
+  res.status(200).json({
+    status: "ok",
+    msg: 'Service ok'
+  })
+});
+
 app.listen(port, function () {
   console.log(`App listening on port: ${port}!`);
 });

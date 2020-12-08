bc09a5330ac4b12d67a9dcd5ad1ddb8aa400208d
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-08 18:31:07 -0300

    add controllers

--- src/api/books.js
+++ src/api/books.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/books", (req, res) => {
   res.status(200).send({
-    message: "Hello World",
+    message: "Hello books",
   });
 });
-
-app.use(require("./books"))
 
 module.exports = app;
